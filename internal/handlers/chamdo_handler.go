20b2bf43c5dfc646607a6ad9f8e7f5ac2de9bd80
klebertonn <kleberr.f@hotmail.com>
2026-06-14 00:28:15 -0300

    add metodos handler e service

--- internal/handlers/chamdo_handler.go
+++ internal/handlers/chamdo_handler.go
@@ -2,42 +2,85 @@ package handlers
 
 import (
 	"net/http"
+	"sistemas-controle-chamdos-internos/internal/models"
+	"sistemas-controle-chamdos-internos/internal/service"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-type ChamadoHendler struct{}
+type ChamadoHendler struct {
+	service *service.ChamadoService
+}
 
-func newChamado() *ChamadoHendler {
-	return &ChamadoHendler{}
+func newChamado(service *service.ChamadoService) *ChamadoHendler {
+	return &ChamadoHendler{
+		service: service,
+	}
 
 }
 
 func (h ChamadoHendler) CriarChamados(c gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
+	var chamado models.Chamado
+
+	if err := c.BindJSON(&chamado); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "dados inválidos",
+		})
+		return
+	}
+
+	if err := h.service.CriarChamados(chamado); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.error(),
+		})
+		return
+	}
+	c.JSON(http.StatusAccepted, gin.H{
 		"message": "Chamado criado com sucesso",
 	})
-
 }
 
 func (h ChamadoHendler) ListarChamados(c gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "lista de chamados",
-	})
+	chamados, err := h.service.ListarChamados()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, chamados)
 
 }
 
 func (h ChamadoHendler) BuscarOrdemID(c gin.Context) {
 	ordemId := c.Param("ordemId")
 
-	c.JSON(http.StatusOK, gin.H{
-		"ordemId": ordemId,
-		"message": "lista de chamados",
-	})
+	ordem, err := strconv.Atooi(ordemId)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Numero de ordem inválida",
+		})
+		return
+	}
+
+	chamado, err := h.service.BuscarOrdemID(ordemId)
+
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "chamado não econtrado",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, chamado)
 
 }
 
-func (h ChamadoHendler) AtualizarChamado(c gin.Context) {
+func (h ChamadoHendler) AtualizarChamado(c *gin.Context) {
 	ordemId := c.Param("ordemId")
 
 	c.JSON(http.StatusOK, gin.H{
--- internal/service/chamado_service.go
+++ internal/service/chamado_service.go
@@ -1,2 +1,26 @@
 package service
 
+type ChamadoService struct {
+	//repository // chamadoRepository
+}
+
+func newChamado() *ChamadoService {
+	return &ChamadoService{}
+
+}
+
+func (s ChamadoService) CriarChamados() {
+
+}
+
+func (s ChamadoService) ListarChamados() ([]string, error) {
+
+}
+
+func (s ChamadoService) BuscarOrdemID() (string, error) {
+
+}
+
+func (s ChamadoService) AtualizarChamado() {
+
+}
