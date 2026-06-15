65ac5b89301154bf56532f6fcdc1ad8ffe8f9fbd
klebertonn <kleberr.f@hotmail.com>
2026-06-15 10:32:56 -0300

    ajuste script sql e database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,54 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"sistemas-controle-chamdos-internos/config"
+	"sistemas-controle-chamdos-internos/database/migrations"
+	"sistemas-controle-chamdos-internos/internal/handlers"
+	"sistemas-controle-chamdos-internos/internal/repositories"
+	"sistemas-controle-chamdos-internos/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+	// Conexão com banco
+	db, err := config.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := migrations.Migrate(db); err != nil {
+		log.Fatal(err)
+	}
+
+	//Repositories
+	chamadoRepo := repositories.NewChamadoRepository(db)
+
+	//Service
+	chamadoService := service.NewChamadoService(chamadoRepo)
+
+	//Handler
+
+	chamadohandler := handlers.NewChamadoHandler(chamadoService)
+
+	//Router
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"menagemTest": "pong",
-		})
-	})
+	r.POST("/chamados", chamadohandler.CriarChamados)
 
-	r.Run(":8080")
+	r.GET("/chamados", chamadohandler.ListarChamados)
+
+	r.GET("/chamados/:ordemId", chamadohandler.BuscarOrdemID)
+
+	r.PUT("/chamados/:ordemId", chamadohandler.AtualizarChamado)
+
+	log.Println("Servidor Iniciado na porta 8080")
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 
 }
